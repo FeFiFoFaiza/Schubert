46198acbf05ea6f300f63b395c26a75890e678b8
Faiza Huda <fhuda30@stuy.edu>
2022-09-25 13:17:35 -0400

    no demos here

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,6 +1,9 @@
 '''
 SS AFK | Anson Wong, Faiza Huda, Kevin Xiao, Truthful Tom, Faizem, FamousMrTable 
-Softdev P2
+Softdev 
+K04 -- Dictionaries
+2022-09-22
+time spent: 0.8
 '''
 '''
 DISCO:
@@ -12,19 +15,59 @@ QCC:
 (especially ones that could shorten what we did)
 - Is there a difference in getting a random devo from a list of all devos vs
 getting a random key and then a random value?
+- How could we change our code to have it work with the original krewes dictionary from the care package?
+- We use packages by doing <package name>.<function in package>
 OPS SUMMARY:
-We decided to make a list of the keys and then randomly choose from that list
-because they aren't numerically sequential. After that, we got a random index for both 
-the list of keys and the list of values. With those, we got a random devo in a random period. 
-'''
 
+Method 1:
+    We added keys to the lists and randomly chose a period and then randomly chose a person from that period
+    using the imported random method
+
+Method 2:
+    We combined all devos into one giant list and randomly selected an index/student.
+    We then used the number of students in each period to see which period said index would into.
+
+'''
+#clean up line 25
 import random
 
-krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']}
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "KAREN"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "WANYING"]
+         }
 
-listofkeys= list(krewes.keys())
-keyIndice = random.randint(0,2)
-valueIndice = random.randint(0,2)
+
+listofkeys= list(krewes.keys()) # for when there are keys - didn't work with original krewes dictionary copied from carepkg
+keyIndice = random.randint(0, len(listofkeys)-1)
+
+valueIndice = random.randint(0, len(krewes[listofkeys[keyIndice]])-1)
+
+print(krewes[listofkeys[keyIndice]][valueIndice] + " Of Period " + str(listofkeys[keyIndice]))
+
+allDevos = []
+for i in krewes:
+    for j in krewes[i]:
+        allDevos.append(j)
+    
+#print(allDevos)
+
+indice = random.randint(0,len(allDevos)-1)
+period = 2
+stud2 = len(krewes[listofkeys[0]])
+stud7 = len(krewes[listofkeys[1]])
+stud8 = len(krewes[listofkeys[2]])
+if (indice > stud2):
+    period = 7
+    if (indice > stud2 + stud7):
+        period = 8
+print(allDevos[indice] + " Of Period " + str(period)) #not sure if this works -Anson
+
+
+# jack and jill, went up the hill, to fetch a pail of water. So they say, the subsequent fall was inevitable, they never stood a chance they were written that way/ 
+
+# original placeholder krewes
+#krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']}
 
 #print(listofkeys)
 
@@ -35,7 +78,6 @@ valueIndice = random.randint(0,2)
 # key values
 #print(krewes[listofkeys[keyIndice]])
 # the random value
-print(krewes[listofkeys[keyIndice]][valueIndice])
 
 # checking if it seems random by running it multiple times
 #for i in range(100):
