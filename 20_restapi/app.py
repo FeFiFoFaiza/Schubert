342ba18201c37c50749680ebf36d16f687b223e1
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2022-11-21 21:33:04 -0500

    Update app.py

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,11 +1,11 @@
-'''
-SEXIST
-
-'''
+# TEAM APPLECAR: Abid Talukder and Faiza Huda and Ben 10 and Truthful Tom
+# SoftDev
+# K20: A RESTful Journey Skyward
+# 2022-11-21
 
 
 from flask import Flask, render_template, request
-import urllib
+import requests
 import json
 
 app = Flask(__name__)
@@ -15,15 +15,14 @@ key = None
 with open('key_nasa.txt') as f:
     key = f.read()
 
-print(key)
-
-
+url = "https://api.nasa.gov/planetary/apod?api_key=" + key
 
 @app.route("/")
 def dispInfo():
-    return True
-
-
+    r = requests.get(url)
+    d = r.json()
+    
+    return render_template("main.html", title=d["title"],i=d["url"], description=d["explanation"])
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
